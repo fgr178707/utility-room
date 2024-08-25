cc9fec775f1a78ed99163782cf71d7700c8d0a23
fgr178707 <fgr178707@163.com>
2024-08-25 19:32:38 +0800

    网站模板

---
+++ HTML/资源网站模板/样式3/scripts.js
@@ -0,0 +1,7 @@
+function detectDarkMode() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add('dark-mode');
+    }
+}
+
+window.addEventListener('load', detectDarkMode);
